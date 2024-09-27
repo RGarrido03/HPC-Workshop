f486a9df864997ded1912d93ffed8db0e0e57c3f
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-09-27 17:36:34 +0100

    Job submission

---
+++ 7/gromacs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH -p fct
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+
+module load gcc11/gromacs/2023
+mpirun -np $SLURM_NTASKS gmx_mpi mdrun -ntomp 1 -maxh .05 -s md.tpr
---
+++ 7/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -p fct
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+
+module load python/3.10.13
+echo "Hello World from bash script!"
+python3 hello_world.py
