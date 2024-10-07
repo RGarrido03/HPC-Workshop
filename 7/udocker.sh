41093c81b7ffb0646bce58da29594bc2b1ba41de
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-08 00:11:10 +0100

    Oops, typoooo

--- 7/udocker.sh
+++ 7/udocker.sh
@@ -7,4 +7,4 @@ module load udocker/1.3.16
 
 udocker pull python:3.12-slim
 udocker create --name=python-test python:3.12-slim
-udocker run -v ~/udocker-test:/app python-test python3 /app/docker.py
+udocker run -v ~/udocker-test:/app python-test python3 /app/udocker.py
